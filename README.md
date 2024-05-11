a436f6f2c89d483c7e585d81d2694691bb34dad9
Sruti <sgsofficial.system@gmail.com>
2024-05-10 18:09:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bot Program
+
+* A simple and preliminary python script for a chat bot.
---
+++ script.py
@@ -0,0 +1,11 @@
+print("Hello! What is your name?")
+name = "Sruti"
+print(f"Nice to meet you, {name}!")
+print("How old are you?")
+age = 27
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old.")
+print("What's your favorite color?")
+color = "red"
+print(f"Oh {color} is a beautiful color.")
